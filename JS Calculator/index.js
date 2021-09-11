175db0b370a96dd2ed321ed499154e9eb02e98f3
Ghost <ghostdeveloper6@gmail.com>
2021-09-11 19:14:16 +0100

    finished the js of the calculator project

--- JS Calculator/index.js
+++ JS Calculator/index.js
@@ -71,7 +71,56 @@ for (let i = 0; i < operator.length; i++) {
 // on click of 'equal to button'
 result.addEventListener("click", function() {
     // this is the string that we will be processing eg. -10+26+33-56*34/23
-  var inputString = input.innerHTML;
+  let inputString = input.innerHTML;
+//   getting all the numbers from the input and saving to an array
+    let numbers = inputString.split(/\+|\-|\×|\÷/g);
 
+    // colleting all the operators from the input.
+    // first we will replace all the numbers and dots with empty strings and then split
+    let operators = inputString.replace(/[0-9]|\./g, "").split("");
+
+    console.log(inputString);
+    console.log(operators);
+    console.log(numbers);
+    console.log('--------------------------');
+
+    // now we will loop through the array and do one operation at a time
+    // following a bodmas: divide, multiply, then subtraction, then addition
+    // 
+    let divide = operators.indexOf("÷");
+    while (divide != -1) {
+        numbers.splice(divide, 2, numbers[divide] / numbers[divide + 1]);
+        operators.splice(divide, 1);
+        divide = operators.indexOf("÷");
+    }
+    let multiply = operators.indexOf("×");
+    while (multiply != -1) {
+        numbers.splice(multiply, 2, numbers[multiply] * numbers[multiply + 1]);
+        operators.splice(multiply, 1);
+        multiply = operators.indexOf("×");
+    }
+    let add = operators.indexOf("+");
+    while (add != -1) {
+        numbers.splice(add, 2, parseFloat(numbers[add]) + parseFloat(numbers[add + 1]));
+        operators.splice(add, 1);
+        add = operators.indexOf("+");
+    }
+    let subtract = operators.indexOf("-");
+    while (subtract != -1) {
+        numbers.splice(subtract, 2, numbers[subtract] - numbers[subtract + 1]);
+        operators.splice(subtract, 1);
+        subtract = operators.indexOf("-");
+    }
+    input.innerHTML = numbers[0];
+    resultDisplayed = true;
     
 });
+
+clear.addEventListener("click", function() {
+    input.innerHTML = "";
+})
+
+// function performOperation (operators, numbers) {
+    
+//     return numbers[0]
+// } 
